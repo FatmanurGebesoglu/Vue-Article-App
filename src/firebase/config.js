b3176aff3c1a7cf95fb3214b32a2de6c532718a5
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-06-05 14:28:54 +0300

    Anasayfa düzenlemeleri yapıldı

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDboYDnfLik8Gj9pTqJTBgJ8k55B6EZauU",
-    authDomain: "vue3-projeler-ffdb4.firebaseapp.com",
-    projectId: "vue3-projeler-ffdb4",
-    storageBucket: "vue3-projeler-ffdb4.appspot.com",
-    messagingSenderId: "857880386349",
-    appId: "1:857880386349:web:dc515b30bcf166dafdd1a4"
-  };
+  apiKey: "AIzaSyDboYDnfLik8Gj9pTqJTBgJ8k55B6EZauU",
+  authDomain: "vue3-projeler-ffdb4.firebaseapp.com",
+  projectId: "vue3-projeler-ffdb4",
+  storageBucket: "vue3-projeler-ffdb4.appspot.com",
+  messagingSenderId: "857880386349",
+  appId: "1:857880386349:web:dc515b30bcf166dafdd1a4"
+};
 
-firebase.initializeApp(firebaseConfig);
-const db=firebase.firestore();
-export {db}
+const db = initializeApp(firebaseConfig);
 
-
+export { db };
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="home">
-   
+    <div v-if="hatalar">
+      {{ hatalar }}
+    </div>
+    <div v-else-if="makaleler.length">
+      <MakaleListesi :makaleler="makaleler"/>
+    </div>
+    <div v-else>
+      Yükleniyor...
+    </div>
   </div>
 </template>
 
 <script>
 
+import MakaleListesi from '@/components/MakaleListesi.vue';
+import makalelerGetir from '@/composables/makalelerGetir';
+
 export default {
   name: 'HomeView',
   components: {
-   
+    MakaleListesi
+  },
+  setup() {
+    const { makaleler, hatalar, makaleListesiniYukle } = makalelerGetir();
+
+    makaleListesiniYukle();
+    return { makaleler, hatalar }
   }
 }
+
+
 </script>
+
+
